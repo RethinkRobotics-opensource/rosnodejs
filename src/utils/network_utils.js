4564798db0a933d950646f4afa672fcc7e839f69
Alexis Schad <schadoc_alex@hotmail.fr>
2018-05-23 17:31:38 +0200

    Check if .match is null instead of its length (#101)
    Better check to alert users if their ROS_IP is set incorrectly somewhere.

--- src/utils/network_utils.js
+++ src/utils/network_utils.js
@@ -66,7 +66,7 @@ let NetworkUtils = {
   getAddressAndPortFromUri(uriString) {
     let regexStr = /(?:http:\/\/|rosrpc:\/\/)?([a-zA-Z\d\-.:]+):(\d+)/;
     let match = uriString.match(regexStr);
-    if (match.length !== 3) {
+    if (match === null) {
       throw new Error ('Unable to find host and port from uri ' + uriString + ' with regex ' +  regexStr);
     }
     // else
